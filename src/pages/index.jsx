8e1686e44e53331883fe5039d3ed28be006088ae
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-10-28 16:54:03 +0700

    add some files

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,18 +5,21 @@ const index = () => {
     return (
         <Fragment>
             <Head>
-                <meta charset='utf-8' />
-                <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-                <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
-                <meta name='description' content='Description' />
-                <meta name='keywords' content='Keywords' />
-                <title>Next.js PWA Example</title>
+            <meta charset='utf-8' />
+            <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+            <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+            <meta name='description' content='Next. js with PWA' />
+            <meta name='keywords' content='Keywords' />
+            <title>Next.js PWA</title>
 
-                <link rel="manifest" href="/manifest.json" />
-                <meta name="theme-color" content="#000000"/>
+            <link rel="manifest" href="/manifest.json" />
+            <link href='/icon.png' rel='icon' type='image/png' sizes='16x16' />
+            <link href='/icon.png' rel='icon' type='image/png' sizes='32x32' />
+            <link rel="apple-touch-icon" href="/icon.png"></link>
+            <meta name="theme-color" content="#000000"/>
             </Head>
             <div>
-                <p>Assalamualaikum</p>
+                <p>Welcome to Next.js with next-pwa</p>
             </div>
         </Fragment>
     )
